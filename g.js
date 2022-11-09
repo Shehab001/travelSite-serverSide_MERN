ccfb5e401f8a35d10caf819f1da58ce5c9ea4fc4
Md. Shehab Chowdhury <shehab15-11396@diu.edu.bd>
2022-11-10 01:26:26 +0600

    r done

---
+++ g.js
@@ -0,0 +1,60 @@
+[
+  {
+    name: "Dhaka to Coxs-bazar",
+    rating: 4.8,
+    total_taken: "88K",
+    price: "৳ 5000",
+    title:
+      "Coxs Bazar is mostly famous for its worlds longest sea beach. Besides, there are dry fishes, burmese products and many high quality international hotel suits and restraunts.",
+    img: "https://res.cloudinary.com/dc9bjecdl/image/upload/v1667940413/assignment111/cox_j66foa.jpg",
+  },
+  {
+    name: "Dhaka to Keokradong",
+    rating: 4.7,
+    total_taken: "68K",
+    price: "৳ 6000",
+    title:
+      "Keokradong is the highest recognized peak of Bangladesh while Tajingdong is the 2nd, Situated in Ruma Upazila both are surrounded by small hills and mountains. ",
+    " img":
+      "https://res.cloudinary.com/dc9bjecdl/image/upload/v1667940413/assignment111/529cd8ed86e1365036f83522b4e91866_dh11cl.jpg",
+  },
+  {
+    name: "Dhaka to Saint-Martin",
+    rating: 4.9,
+    total_taken: "428K",
+    price: "৳ 8000",
+    title:
+      "Saint Martins Island is one of the best tourist spot. Saint Martin’s Island is the only coral island in Bangladesh. It is a small island in the North-Eastern part of the Bay of Bengal.",
+    " img":
+      "https://res.cloudinary.com/dc9bjecdl/image/upload/v1667940412/assignment111/a3af85fcc8b487cb7677352e06cd1f3f_h1r8ua.jpg",
+  },
+  {
+    name: "Dhaka to Kuakata",
+    rating: 4,
+    total_taken: "8K",
+    title: "",
+    price: "৳ 3000",
+    " img":
+      "https://res.cloudinary.com/dc9bjecdl/image/upload/v1667940407/assignment111/6230816_Kuakata_beach_Bangladesh_zytmww.jpg",
+  },
+  {
+    name: "Dhaka to Hamham Waterfall",
+    rating: 5,
+    total_taken: "188K",
+    title:
+      "Saint Martins Island is one of the best tourist spot. Saint Martin’s Island is the only coral island in Bangladesh. It is a small island in the North-Eastern part of the Bay of Bengal.",
+    price: "৳ 3000",
+    " img":
+      "https://res.cloudinary.com/dc9bjecdl/image/upload/v1667940412/assignment111/Hamham-waterfall-in-Srimang_qkg9my.jpg",
+  },
+  {
+    name: "Dhaka to Sundarban",
+    rating: 4.2,
+    total_taken: "288K",
+    price: "৳ 5000",
+    title:
+      "The Sundarbans is a vast forest on the coast of the Bay of Bengal. Sundarban is located in both Bangladesh and West Bengal of India.",
+    " img":
+      "https://res.cloudinary.com/dc9bjecdl/image/upload/v1667940412/assignment111/ce_ekws8n.jpg",
+  },
+];
--- index.js
+++ index.js
@@ -1,13 +1,11 @@
 const express = require("express");
 const app = express();
 const cors = require("cors");
+const { MongoClient, ServerApiVersion } = require("mongodb");
 const port = process.env.PORT || 5000;
 
 app.use(cors());
-// travel-site
-// E9Ig2KTJZLVNZnvd
 
-const { MongoClient, ServerApiVersion } = require("mongodb");
 const uri =
   "mongodb+srv://travel-site:E9Ig2KTJZLVNZnvd@cluster0.8lf54jt.mongodb.net/?retryWrites=true&w=majority";
 const client = new MongoClient(uri, {
@@ -15,71 +13,29 @@ const client = new MongoClient(uri, {
   useUnifiedTopology: true,
   serverApi: ServerApiVersion.v1,
 });
-client.connect((err) => {
-  const collection = client.db("test").collection("devices");
-  // perform actions on the collection object
-  console.log("connected");
-});
 
 // const categories = require("./data/categories.json");
 // const news = require("./data/news.json");
 
-// async function run() {
-//   try {
-//     const productCollection = client.db("emaJohn").collection("products");
+async function run() {
+  try {
+    const userCollection = client.db("travel-site");
 
-//     app.get("/products", async (req, res) => {
-//       const page = parseInt(req.query.page);
-//       const size = parseInt(req.query.size);
-//       console.log(page, size);
-//       const query = {};
-//       const cursor = productCollection.find(query);
-//       const products = await cursor
-//         .skip(page * size)
-//         .limit(size)
-//         .toArray();
-//       const count = await productCollection.estimatedDocumentCount();
-//       res.send({ count, products });
-//     });
+    app.get("/services", async (req, res) => {
+      const services = userCollection.collection("serviceDetails");
 
-//     app.post("/productsByIds", async (req, res) => {
-//       const ids = req.body;
-//       const objectIds = ids.map((id) => ObjectId(id));
-//       const query = { _id: { $in: objectIds } };
-//       const cursor = productCollection.find(query);
-//       const products = await cursor.toArray();
-//       res.send(products);
-//     });
-//   } finally {
-//   }
-// }
-// run().catch((err) => console.error(err));
-// app.get("/", (req, res) => {
-//   res.send("Brins API Running");
-// });
-
-// app.get("/categories", (req, res) => {
-//   res.send(categories);
-// });
-
-// app.get("/category/:id", (req, res) => {
-//   const id = req.params.id;
-//   //console.log(id);
-
-//   const category_news = news.filter((n) => n.category_id === id);
-//   //console.log(n.category_id);
-//   res.send(category_news);
-// });
+      const cursor = services.find({});
+      const users = await cursor.toArray();
+      res.send(users);
+    });
+  } finally {
+  }
+}
+run().catch((err) => console.error(err));
 
 app.get("/", (req, res) => {
   res.send("korrah");
 });
-
-// app.get('/news/:id', (req, res) => {
-//     const id = req.params.id;
-//     const selectedNews = news.find(n => n._id === id);
-//     res.send(selectedNews);
-// });
 
 app.listen(port, () => {
   console.log("Travel Side Server running on port", port);
